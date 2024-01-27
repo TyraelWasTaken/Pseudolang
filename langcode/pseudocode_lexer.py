d4c7436b00803fdbf2e6cec3915889bb3dcc901a
blokdev <blokywocky@gmail.com>
2024-01-27 06:52:13 +0000

    Basic math (lexer only)

--- langcode/pseudocode_lexer.py
+++ langcode/pseudocode_lexer.py
@@ -1,3 +1,49 @@
+types = {
+    "+": "AO",
+    "-": "AO",
+    "*": "AO",
+    "/": "AO",
+    "MOD": "AO",
+    "DIV": "AO"
+}
+
+'''
+AO = Arithmatic Operator
+'''
+
+
 def lex(data):
-    tokens = {}
+    content = list(data)
+    
+    tokens = []
+    currentToken = {}
+    state = 0   # will be 1 if a string has started or something smh
+    tok = ""
+    
+    for char in content:
+        
+        if char == " " or char == ";" and state == 0: # we will need to put a ";" at the end of each line unless we use the \n when a new line starts :p
+            
+            if tok.isnumeric():
+                
+                currentToken = {
+                    "image": tok,
+                    "type": "integer"
+                }
+                tokens.append(currentToken)
+                currentToken = {}
+            
+            tok = ""
+            
+        elif char in types and types[char]  == "AO" and state == 0:
+            currentToken = {
+                "image": char,
+                "type": "AO"
+            }
+            tokens.append(currentToken)
+            tok = ""
+        else:
+            tok += char
+            
+        
     return tokens
--- main.py
+++ main.py
@@ -4,27 +4,10 @@ def openfile(filename):
     data = open(filename, "r").read()
     return data
 
-def lex(data):
-    content = list(data)
-    
-    
-    tokens = {}
-    currentToken = {}
-    state = 0   #will be 1 if a string has started
-    tok = ""
-    
-    for char in content:
-        if char == " " and state == 0:
-            print(list(tok))
-            tok = ""
-        else:
-            tok += char
-            
-        
-    return tokens
-
 if __name__ == "__main__":
     data = openfile("test.aqa")
     
-    tokens = lex(data)
-    print(tokens)
+    tokens = langcode.pseudocode_lexer.lex(data)
+    print(tokens) 
+    
+    # there has gotta be a better way to do the lexer stuff lmao

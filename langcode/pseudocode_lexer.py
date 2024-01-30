9c5ab269cdd345e7ae32441f861e52089a9b6dff
blokongithub <blokongithub@gmail.com>
2024-01-30 14:31:16 +0000

    test

--- langcode/pseudocode_lexer.py
+++ langcode/pseudocode_lexer.py
@@ -57,35 +57,76 @@ class lex():
         currentToken = {}
         
         self.tok = ""
+        
+    def stringything(self):
+        if self.stringstart == False:
+            self.stringstart = True
+            self.tok = ""
+        elif self.stringstart == True:
+            self.add_token(image=self.tok[:-1], token_type="STRING")
+            self.stringstart = False
+        
+    def lex(self, data):
+        content = list(data)
+        arrow = ""
+        for char in content:
+            self.tok += char
+            
+            if char == " " and self.stringstart == False:
+                if self.tok == " ":
+                    self.tok = ""
+                elif self.tok[:-1].isnumeric():
+                    self.add_token(image=self.tok[:-1], token_type="INTEGER")
+                elif self.isfloaty(self.tok[:-1]) == True:
+                    self.add_token(image=self.tok[:-1], token_type="FLOAT")
+                else:
+                    self.add_token(image=self.tok[:-1], token_type="VAR")
+                    
 
-    def arithmatic(self):
-        if types[self.tok] == "AO":
             
-            currentToken = {
-                "image": self.tok,
-                "type": "AO"
-            }
+            if self.tok in self.types:
+                self.add_token(image=self.tok, token_type=self.types[self.tok])
+                        
+            if char == "\"":
+                self.stringything()
             
-        elif types[self.tok] == "OUT":
-            currentToken = {
-                "type": "OUT"
-            }
-            
-        self.tokens.append(currentToken) 
-        currentToken = {}
-        self.tok = ""
+            '''
+            if char == '\n':
+                self.add_token(image='EOL', token_type='EOL')
+            '''
 
-    def string(self):
-        if stringstart == False:
-            stringstart = True
-        elif stringstart == True:
-            currentToken = {
-                    "image": self.tok,
-                    "type": "STRING"
-                }
-            self.tokens.append(currentToken) 
-            currentToken = {}
-            stringstart = False
-        self.tok = ""
-                           
+            if char == '\n' and self.line != []:   
+                if self.tok[0:-1].isnumeric():
+                    self.add_token(image=self.tok[0:-1], token_type="INTEGER")
+                elif self.isfloaty(self.tok[0:-1]) == True:
+                    self.add_token(image=self.tok[0:-1], token_type="FLOAT")
+                    
+                self.tokens.append(self.line)
+                self.line = []
+                self.tok = ""
+            if self.tok == '\n':
+                self.tok = ""
+            #print(self.line)
+            
+            if char == "<" or char == "-" and arrow != "":
+                arrow += char
+                if arrow == "<--":
+                    del self.line[-3:]
+                    self.add_token(image="<--", token_type="ARRW")
+                    arrow = ""
+                
+        if self.line != []: #checks if a line has been entered with nun on
+            if self.tok[0:].isnumeric():
+                self.add_token(image=self.tok[0:], token_type="INTEGER")
+            elif self.isfloaty(self.tok[0:]) == True:
+                self.add_token(image=self.tok[0:], token_type="FLOAT")
+            self.tokens.append(self.line)
+
+        self.add_token(image='EOL', token_type='EOL')
+        return self.tokens
+
+        
+    
+#Parsers
+#    def Parser(self):
         
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import langcode.pseudocode_lexer
+<<<<<<< Updated upstream
+=======
+import json
+>>>>>>> Stashed changes
 
 def openfile(filename):
     data = open(filename, "r").read()
     return data
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     data = openfile("C:/Users/samue/OneDrive/Documents/code/Psuedolang/Pseudolang/Pseudolang/Testing/test.aqa")
     
     tokens = langcode.pseudocode_lexer.lex(data)
     print(tokens) 
     
     # there has gotta be a better way to do the lexer stuff lmao
+=======
+    lexer = langcode.pseudocode_lexer.Lexer()
+    data = openfile("test.aqa")
+    tokens = lexer.lex(data)
+    print(tokens)
+
+    with open("x.json", "w") as a:
+        json.dump(tokens, a, indent=4) #easyer to read
+>>>>>>> Stashed changes
